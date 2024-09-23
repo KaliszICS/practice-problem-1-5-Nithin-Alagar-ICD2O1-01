6a0340e57b8f5c0fab9fd0980ec05dff6beebadb
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-09-23 13:51:04 +0000

    nithin

--- PP1_5.py
+++ PP1_5.py
@@ -1,9 +1,13 @@
 
 def q1():
   #Write Assignment code here
-
+  a = int(input("Input an integer: "))
+  print(a + 3)
 def q2():
   #Write Assignment code here
+  b = int(input("Input an integer: "))
+  c = float(str(b)) + 3
+  print(c)
 
 def q3():
   #Write Assignment code here
