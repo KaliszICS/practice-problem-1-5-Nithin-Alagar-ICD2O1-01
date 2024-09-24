d0dc282b301f7bc14e04913779cae40801a43c2f
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-09-24 13:41:27 +0000

    nithin

--- PP1_5.py
+++ PP1_5.py
@@ -1,24 +1,29 @@
 
 def q1():
   #Write Assignment code here
-  a = int(input("Input an integer: "))
+  a = input("Input an integer: ")
+  a = int(a)
   print(a + 3)
 def q2():
   #Write Assignment code here
-  b = int(input("Input an integer: "))
-  c = float(str(b + "4")) + 2
-  print(c)
-
+  user1 = input("Input a number: ")
+  a = str(user1) + "4"
+  a = float(a)+2
+  print(a)
 def q3():
   #Write Assignment code here
   radius = float(input("Input a radius: "))
-  a = float((radius**2)*3.14)
+  area = float((radius**2)*3.14)
+  print(area)
 def q4():
   #Write Assignment code here
-  user1 = float(input("Input a number: "))*12
-  str(print(user1))
+  user5 = float(input("Input a number: "))
+  t = int(user5 * 12)
+  print(t)
 
 def q5():
   #Write Assignment code here
-  print(a)
+  user5 = (input("Input an integer: "))
+  result = int(user5) + 5
+  print(f'Your number + 5 is {result}')
   
